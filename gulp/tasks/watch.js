4fb36a4ac4ca4a8a9e57c30d3e6bb629dead090e
Timon Wong <wangtianpeng@nhn.com>
2015-01-06 22:54:29 +0800

    Fix watch target name

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -14,7 +14,7 @@ gulp.task('watch', ['watchify', 'browserSync'], function () {
   }))
   gulp.watch(copySrcs, ['copy'])
 
-  gulp.watch(config.sass.src, ['styles'])
+  gulp.watch(config.sass.src, ['sass'])
   gulp.watch(config.images.src, ['images'])
   gulp.watch(config.markup.src, ['markup'])
   // Watchify will watch and recompile our JS, so no need to gulp.watch it
